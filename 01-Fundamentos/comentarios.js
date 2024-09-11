586b84120904f3ce40d8a8594df75fd8184a547b
PaxiSuru <163039847+PaxiSuru@users.noreply.github.com>
2024-09-11 13:04:57 -0600

    Comentarios y Variables en Javascript

---
+++ 01-Fundamentos/comentarios.js
@@ -0,0 +1,19 @@
+// Comentarios
+
+/*
+Comentaríos en bloque
+
+Para más de una linea de comentarios.
+Usamos el shortcut shift + alt + a
+*/
+
+
+//La función alert () lanza una ventana de alerta
+alert ("Estoy enlazado");
+/* 
+Recomendaciones
+
+1. No debemos recargar mucho nuestro código de comentarios.
+2. Los comentarios deben ser concretos y descriptivos.
+3. No deben ser redundantes respecto al código.
+*/
---
+++ 01-Fundamentos/variables.js
@@ -0,0 +1,45 @@
+/* 
+Variables
+
+Las variables son contenedores que almacenan datos.
+
+Las variables se declaran usando las palabras reservadas:
+
+var -> Es la forma tradicional, pero ya no se recomienda.
+let -> Es la forma moderna, es la recomendada.
+const -> El dato almacenado no puede cambiar.
+
+Para declarar una variable:
+
+var [nombreVariable] = [valor];
+*/
+
+var papitas = "Pao Piña";
+let edad = 23;
+const vivo = true;
+
+console.log(papitas)
+console.log(edad)
+console.log(vivo);
+
+/* 
+ Las variables declaradas con 'var' y 'let' pueden cambiar a lo largo del código, siempre y cuando su alcance lo permita.
+
+  Las variables declaradas con 'const' no pueden cambiar. Si lo intentamos marcara un error.
+
+  Para cambiar el valor de una variable:
+
+  [NombreVariable] = [NuevoValor]
+*/
+
+nombre = "maruchan";
+
+
+//Mostrar los nuevos datos.
+console.log(nombre);
+
+edad = "26";
+console.log(edad);
+
+/* vivo = "false"
+console.log(vivo); */
