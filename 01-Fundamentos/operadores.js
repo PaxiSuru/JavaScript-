6ed1abda89dc9e0d5218715ff92a1ab02a7a5cd2
PaxiSuru <163039847+PaxiSuru@users.noreply.github.com>
2024-09-25 13:01:08 -0600

    Aprendimos operadores de asginación y comparación

--- 01-Fundamentos/operadores.js
+++ 01-Fundamentos/operadores.js
@@ -60,3 +60,86 @@ console.log(x);
 let y= 6;
 y--; //5
 console.log(y)
+
+/* 
+ 2. Operadores de Asignación
+
+ Estos operadores asignan valores a las variables.
+*/
+
+//Asignación (=) Asigna un valor a una variable.
+
+let w = 45;
+
+//Asignación con suma (+=) Suma el valor a una variable y la reasigna.
+
+w += 5; //50
+console.log(w);
+
+// Asignación con resta (-=) Resta el valor a una variable y le reasigna.
+
+w -= 15; //35
+console.log(w)
+
+// Asignación con Multiplicación (*=) Multiplica una variable por valor y la reasigna.
+
+w *= 2; //70
+console.log(w)
+
+// Asignación con división (/=) Divide una variable por un valor y la reasigna.
+
+w /= 10; //7
+console.log(w)
+
+/* 
+ 3. Comparación
+  Estos operadores comparan dos valores y devuelven 'true' o 'false'.
+*/
+
+//Igualdad (==) Compara dos valores para ver si son iguales. NOTA: Sin tener en cuenta el tipo de dato.
+
+let valor = "5";
+let compara = 5 == valor;
+console.log(compara) //true
+
+//Igualdad estricta (===) Comparar dos valores y su tipo para ver si son exactamente iguales.
+
+compara = 5 === valor;
+console.log(compara) //valor
+
+//Desigualdad (!=) Verificar si dos valores no son iguales.
+
+let valor1 = "10";
+let valor2 = 10;
+
+console.log(valor1 != valor2); //false
+
+//desigualdad estricta (!==) Verifica si dos valores y sus tipos no son exactamente iguales.
+
+valor1 = "10";
+valor2 = 10;
+
+console.log(valor1 !== valor2); //true
+
+//Mayor que (>) verifica si el primer valor es mayor que el segundo.
+
+compara = 10 > 5;
+console.log(compara); //true
+
+//Mayor o igual que (>=) verificar si el primer valor es mayor o igual que el segundo.
+
+compara = 5 >= 5;
+console.log(compara); //true
+
+//Menor que (<) verifica si el primer valor es menor que el segundo.
+
+console.log(3 < 8); //true
+console.log(10 < 8); //false
+console.log(5 < 5); //false
+
+//Menor o igual que (<=) verifica si el primer valor es menor o igual que el segundo valor.
+
+console.log(4 <= 10);
+console.log(10 <= 100);
+console.log(45 <= 7);
+console.log(8 <= 8);
